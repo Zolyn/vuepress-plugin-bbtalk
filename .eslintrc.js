43f44f85250f9c85567a42ae614f7d672dadd753
Zorin <pikasama@qq.com>
2021-05-09 20:33:34 +0800

    chore: Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: ['alloy', 'alloy/vue'],
+    env: {},
+    globals: {},
+    rules: {
+        'vue/component-tags-order': [
+            'error',
+            {
+                order: [['template', 'script'], 'style'],
+            },
+        ],
+    },
+};
