6297f05804c13eced347774c895785f3aa4757c4
Zorin <pikasama@qq.com>
2021-05-19 23:10:59 +0800

    test: bbtalk test

---
+++ index.js
@@ -0,0 +1,12 @@
+const { resolve } = require('path');
+
+module.exports = (opts) => ({
+    define() {
+        return {
+            PLUGIN_CONFIG: opts.plugin,
+            BBTALK_CONFIG: opts.bbtalk,
+        };
+    },
+    name: '@Zolyn/vuepress-plugin-bbtalk',
+    enhanceAppFiles: resolve(__dirname, './src/enhanceApp.js'),
+});
---
+++ src/BBTalk.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="vuepress-plugin-bbtalk"></div>
+</template>
+
+<script>
+const bbtalkVersion = '0.1.6';
+
+export default {
+    data() {
+        return {
+            pNode: PLUGIN_CONFIG.parentNode || '.page',
+            bbtalkConfig: BBTALK_CONFIG,
+        };
+    },
+    mounted() {
+        this.$nextTick(() => {
+            const bbtalkScript = document.createElement('script');
+            bbtalkScript.src = `https://cdn.jsdelivr.net/npm/bbtalk@${bbtalkVersion}/dist/bbtalk.min.js`;
+            document.body.appendChild(bbtalkScript);
+            this.initBBTalk();
+        });
+    },
+    methods: {
+        initBBTalk() {
+            const bbtalkContainer = document.createElement('div');
+            bbtalkContainer.id = this.bbtalkConfig.el;
+            document.querySelector(this.pNode).appendChild(bbtalkContainer);
+            bbtalk.init({ ...this.bbtalkConfig });
+        },
+    },
+};
+</script>
+
+<style scoped></style>
